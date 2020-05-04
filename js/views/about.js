06c4cef363265df1cb370c41c3df203e5be114d0
Ilya Storchevoy <Ilya.Storchevoy@x5.ru>
2020-05-04 22:16:57 +0300

    About page

--- js/views/about.js
+++ js/views/about.js
@@ -2,5 +2,18 @@ export default {
     template:
         '<v-row align="center" justify="center">' +
             '<h1 class="display-2 white--text">About</h1>' +
+            '<v-card class="mt-5">' +
+                '<v-card-text>' +
+                    'Hello there!' +
+                    '<br>' +
+                    'This project start as experiment for create the simple website on vue.js, using only a cdn-distributable libraries, without any bundlers or compilers.\n' +
+                    'This was possible with <strong>type="module"</strong> feature of <strong>sctipt</strong> tag. In short, this feature allows me using import and export syntax right in browser. \n' +
+                    'More information you can find <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules" target="_blank">here</a>.' +
+                    '<br>' +
+                    'I kept this project on the shelf, but now I found enough time to rise it up. I planing making posts about new programming languages features, that I found, tasks solutions, some of my gists, etc.' +
+                    '<br>' +
+                    'You can find source code on github page <a href="https://github.com/JamesDeGreese/md-blog">https://github.com/JamesDeGreese/md-blog</a>' +
+                '</v-card-text>' +
+            '</v-card>' +
         '</v-row>'
 }
